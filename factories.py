a63d858d37909bea916da94c7ea9b4e02cdf3d95
Leon Eiböck <eiboeck.leon@gmail.com>
2026-07-18 11:42:55 +0200

    Documentation without tests

--- factories.py
+++ factories.py
@@ -1,3 +1,7 @@
+"""
+Module which contains multiple classes for creating graph elements
+"""
+
 __autor__ = "Leon Eiböck"
 __date__ = "17/07/2026"
 __license__ = "GNU GPLv3"
@@ -5,10 +9,21 @@ __status__ = "In development"
 
 
 class NodeFactory:
+    """
+    Class which creates nodes based on node groups
+    """
+
     _registry: dict[str, type] = {}
 
     @classmethod
     def register_role(cls, role: str):
+        """
+        Decorator to register a role
+        :param role: to be registered
+        :return: new Function
+        @TODO doctests/unittests
+        """
+
         def wrapper(func):
             cls._registry[role] = func
             return func
@@ -16,13 +31,32 @@ class NodeFactory:
         return wrapper
 
     def create_node(self, image: str, role: str, name: str) -> GenericNode:
+        """
+        Create a new node based on role.
+        :param image: which image to use for this node
+        :param role: which role to use for this node
+        :param name: which name to use for this node
+        :return: new Node
+        @TODO doctests/unittests
+        """
         node = self._registry[role](image, name)
         return node
 
 
 class EdgeFactory:
+    """
+    Class which connects the nodes based on given edge groups
+    """
+
     @staticmethod
     def create_edge(edge_group: list, nodes: dict[str, GenericNode]) -> Edge:
+        """
+        Adds new Interfaces to both nodes and connects them via a new edge
+        :param edge_group: list with two Nodes and two Interfaces
+        :param nodes: Dictionary of Nodes to resolve name of nodes
+        :return:
+        @TODO doctests/unittests
+        """
         intf_1 = nodes[edge_group[0]].add_interface(edge_group[1])
         intf_2 = nodes[edge_group[2]].add_interface(edge_group[3])
         edge = Edge(intf_1, intf_2)
@@ -32,52 +66,129 @@ class EdgeFactory:
 
 
 class Interface:
+    """
+    Object which is used to connect Nodes and Edges
+    """
+
     def __init__(self, if_name: str, node: GenericNode):
+        """
+        Initializes the Interface class
+        :param if_name: name of the interface
+        :param node: Node which owns this interface
+        @TODO doctests/unittests
+        """
         self._if_name = if_name
         self._node = node
 
     @property
     def name(self) -> str:
+        """
+        Returns the name of the interface
+        :return:
+        @TODO doctests/unittests
+        """
         return self._if_name
 
     @property
     def node(self) -> GenericNode:
+        """
+        Returns the Node which owns this interface
+        :return:
+        @TODO doctests/unittests
+        """
         return self._node
 
     @property
     def edge(self):
+        """
+        Returns the Edge which is connected to this interface
+        :return:
+        @TODO doctests/unittests
+        """
         return getattr(self, "_edge", None)
 
     @edge.setter
     def edge(self, edge: Edge) -> None:
+        """
+        Sets the Edge which is connected to this interface
+        :param edge:
+        :return:
+        @TODO doctests/unittests
+        """
         self._edge = edge
 
-    def __str__(self) -> str:
-        return f"{self.name}"
-
     def __repr__(self) -> str:
+        """
+        Makes object into a string. Can be used in eval()
+        :return:
+        @TODO doctests/unittests
+        """
         return f"Interface({self.name}, {self.node.name})"
+
+    def __str__(self) -> str:
+        """
+        Makes object into a more human-readable string.
+        :return:
+        @TODO doctests/unittests
+        """
+        return f"{self.name}"
 
 
 class Edge:
+    """
+    Object which is used to connect interfaces of nodes
+    """
+
     def __init__(self, interface_1: Interface, interface_2: Interface):
+        """
+        Initializes the Edge class
+        :param interface_1: to be connected to other interface
+        :param interface_2:  to be connected to other interface
+        @TODO doctests/unittests
+        """
         self.incidence_1 = interface_1
         self.incidence_2 = interface_2
 
     def __repr__(self) -> str:
+        """
+        Makes object into a string. Can be used in eval()
+        :return:
+        @TODO doctests/unittests
+        """
         return f"Edge({repr(self.incidence_1)}, {repr(self.incidence_2)})"
 
     def __str__(self) -> str:
+        """
+        Makes object into a more human-readable string.
+        :return:
+        @TODO doctests/unittests
+        """
         return f"{self.incidence_1.node} <--> {self.incidence_2.node}"
 
 
 class GenericNode:
+    """
+    Node object to save each node in the graph. Is the parent object for other nodes.
+    """
+
     def __init__(self, image: str, name: str):
+        """
+        Initializes the Node class
+        :param image: which image to use for this node
+        :param name: which name to use for this node
+        @TODO doctests/unittests
+        """
         self.image = image
         self.name = name
         self._interfaces = {}
 
     def add_interface(self, if_name: str) -> Interface:
+        """
+        Adds an interface to the graph. Raises an exception if the interface name already exists
+        :param if_name: Name of new Interface
+        :return: added Interface
+        @TODO doctests/unittests
+        """
         if if_name in self._interfaces:
             raise ValueError(f"Interface {if_name} already exists on node {self.name}")
         intf = Interface(if_name, self)
@@ -85,32 +196,67 @@ class GenericNode:
         return intf
 
     def __repr__(self):
+        """
+        Makes object into a string. Can be used in eval()
+        :return:
+        @TODO doctests/unittests
+        """
         return f"{self.__class__.__name__}({self.image}, {self.name})"
 
     def __str__(self):
+        """
+        Makes object into a more human-readable string.
+        :return:
+        @TODO doctests/unittests
+        """
         return f"{self.name}"
 
 
 @NodeFactory.register_role("PC")
 class PC(GenericNode):
+    """
+    Node object which represents the role PC.
+    @TODO doctests/unittests
+    """
+
     pass
 
 
 @NodeFactory.register_role("VM")
 class VM(GenericNode):
+    """
+    Node object which represents the role VM.
+    @TODO doctests/unittests
+    """
+
     pass
 
 
 @NodeFactory.register_role("SWITCH")
 class Switch(GenericNode):
+    """
+    Node object which represents the role SWITCH.
+    @TODO doctests/unittests
+    """
+
     pass
 
 
 @NodeFactory.register_role("ROUTER")
 class Router(GenericNode):
+    """
+    Node object which represents the role ROUTER.
+    @TODO doctests/unittests
+    """
+
     pass
 
 
 @NodeFactory.register_role("FW")
 class Firewall(GenericNode):
+    """
+    Node object which represents the role FW.
+    @TODO doctests/unittests
+    """
+
     pass
--- graph_builder.py
+++ graph_builder.py
@@ -7,12 +7,27 @@ from factories import NodeFactory, EdgeFactory, GenericNode
 
 
 class GraphBuilder:
+    """
+    Facade of factories to build easier graphs
+    """
+
     def __init__(self, nodes, edges):
+        """
+        Initializes the GraphBuilder class
+        :param nodes: dict of nodes from the config file
+        :param edges: list of edges from the config file
+        @TODO doctests/unittests
+        """
         self._config_nodes = nodes
         self._config_edges = edges
         self._nodes = {}
 
     def _build_nodes(self):
+        """
+        Builds all the nodes in the graph. Saves the result to self._nodes
+        :return:
+        @TODO doctests/unittests
+        """
         for node_group in self._config_nodes:
             image = node_group["image"]
             role = node_group["role"]
@@ -20,12 +35,22 @@ class GraphBuilder:
                 self._nodes[name] = NodeFactory().create_node(image, role, name)
 
     def _build_edges(self):
+        """
+        Builds all the edges in the graph. In-Place operation for self._nodes
+        :return:
+        @TODO doctests/unittests
+        """
         if not self._nodes:
             raise ValueError("No nodes created. Use first NodeFactory.build_nodes()")
         for edge in self._config_edges:
             EdgeFactory().create_edge(edge, self._nodes)
 
     def build(self) -> dict[str, GenericNode]:
+        """
+        Builds the graph
+        :return: new graph
+        @TODO doctests/unittests
+        """
         self._build_nodes()
         self._build_edges()
         return self._nodes
